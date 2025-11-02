095898b06090ff304d7e77176c31af0d30629ff2
Olajide <oolajide91@gmail.com>
2025-11-02 16:10:15 +0100

    docker-compose file update

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),  # fallback to 'postgres' for Docker
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'PORT': os.getenv('POSTGRES_PORT, 5432'),
     }
 }
 # Cache
