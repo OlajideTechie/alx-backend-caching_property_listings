8177165a98fbf85e29a4ebde1a35fe42ed222865
Olajide <oolajide91@gmail.com>
2025-11-02 16:29:44 +0100

    total request metrics fix

--- properties/utils.py
+++ properties/utils.py
@@ -25,8 +25,8 @@ def get_redis_cache_metrics():
         keyspace_hits = info.get("keyspace_hits", 0)
         keyspace_misses = info.get("keyspace_misses", 0)
 
-        total = keyspace_hits + keyspace_misses
-        hit_ratio = (keyspace_hits / total) if total > 0 else 0
+        total_requests = keyspace_hits + keyspace_misses
+        hit_ratio = (keyspace_hits / total_requests) if total_requests > 0 else 0
 
         metrics = {
             "keyspace_hits": keyspace_hits,
